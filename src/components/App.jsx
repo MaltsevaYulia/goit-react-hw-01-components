250641033ea85cd689e6be8f2a22b415942659f1
MaltsevaYulia <112495890+MaltsevaYulia@users.noreply.github.com>
2023-03-15 12:53:56 +0200

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
+import { Profile } from './Profile';
+import user from '../user.json'
+import data from '../data.json'
+import { Statistics } from './Statistics';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+    <Profile user={user}/>
+    <Statistics data={data}/>
+    </>
+    // <div
+    //   style={{
+    //     height: '100vh',
+    //     display: 'flex',
+    //     justifyContent: 'center',
+    //     alignItems: 'center',
+    //     fontSize: 40,
+    //     color: '#010101'
+    //   }}
+    // >
+    //   React homework template
+    // </div>
   );
 };
